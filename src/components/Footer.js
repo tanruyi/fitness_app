d8e2383b62c77fbd69300e613cb216c6bf18faba
ruyi <tan_ruyi@hotmail.com>
2022-10-03 21:57:59 +0800

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,20 @@
 import React from 'react';
 import {Box, Stack, Typography} from '@mui/material';
 
+// IMPORT IMAGES
+import Logo from '../assets/images/Logo-1.png';
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt="80px" backgroundColor="#FFF3F4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px" />
+
+        <Typography variant='h5' pb="40px" mt="20px">
+          Made with 💖 by Ru Yi
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
