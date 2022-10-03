3d661735d19c35462be55441ab28a6ca588d6dc9
ruyi <tan_ruyi@hotmail.com>
2022-10-03 11:13:04 +0800

    Search function - done

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -10,7 +10,18 @@ import { exerciseOptions, fetchData } from '../utilities/fetchData';
 import ExerciseCard from './ExerciseCard';
 
 const Exercises = (props) => {
-    console.log(props.exercises);
+    const [currentPage, setCurrentPage] = useState(1);
+    const exercisesPerPage = 9;
+
+    const indexOfLastExercise = currentPage * exercisesPerPage;
+    const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+    const currentExercises = props.exercises.slice(indexOfFirstExercise, indexOfLastExercise)
+
+    const paginate = (e, value) => {
+        setCurrentPage(value);
+
+        window.scrollTo({top: 1800, behavior: "smooth"});
+    }
 
   return (
     <Box id="exercises" mt="50px" p="20px"
@@ -21,9 +32,15 @@ const Exercises = (props) => {
         </Typography>
 
         <Stack direction="row" flexWrap="wrap" justifyContent="center" sx={{gap: {xs: "50px", lg: "110px"}}}>
-            {props.exercises.map((exercise, index) => (
+            {currentExercises.map((exercise, index) => (
                 <ExerciseCard key={index} exercise={exercise} />
             ))}
+        </Stack>
+
+        <Stack mt="100px" alignItems="center">
+            {props.exercises.length > exercisesPerPage && (
+                <Pagination shape="rounded" size="large" count={Math.ceil(props.exercises.length / exercisesPerPage)} page={currentPage} onChange={paginate} />
+            )}
         </Stack>
     </Box>
   )
