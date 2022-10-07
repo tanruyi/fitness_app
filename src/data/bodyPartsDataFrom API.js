4f32461d8c45cfdb723f336e0e735353fd016f80
ruyi <tan_ruyi@hotmail.com>
2022-10-07 15:05:07 +0800

    Revert "removed old code"
    This reverts commit 77100a3e662cbe6274e727c05711942dd7b7cfec.

--- src/data/bodyPartsDataFrom API.js
+++ src/data/bodyPartsDataFrom API.js
@@ -1,5 +1,10 @@
 // IMPORT CUSTOM HOOKS 
 import { exerciseOptions, fetchData } from '../utilities/fetchData';
 
+// DATA FROM API CALLED IN SEARCHEXERCISES COMPONENT FOR HORIZONTAL SCROLLBAR CHILD COMPONENT
+// export const bodyPartsDataFromAPI = ['all', 'back', 'cardio', 'chest', 'lower arms', 'lower legs', 'neck', 'shoulders', 'upper arms', 'upper legs', 'waist'];
+
 // get list of body parts from API
 export const bodyPartsDataFromAPI = fetchData("https://exercisedb.p.rapidapi.com/exercises/bodyPartList", exerciseOptions);
+
+
